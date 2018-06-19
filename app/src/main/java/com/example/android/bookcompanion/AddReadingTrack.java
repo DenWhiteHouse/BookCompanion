e17db0396c524138acbe1804e960a72e9a918429
Denny Casabianca <casabiancadenny@gmail.com>
2018-06-19 12:36:48 +0200

    DatePicker Fixed

--- app/src/main/java/com/example/android/bookcompanion/AddReadingTrack.java
+++ app/src/main/java/com/example/android/bookcompanion/AddReadingTrack.java
@@ -1,15 +1,19 @@
 package com.example.android.bookcompanion;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -36,9 +40,10 @@ public class AddReadingTrack extends AppCompatActivity {
     @BindView(R.id.saveReadingTrackButton) Button mSaveReadingTrackButton;
     ReadingTrackDatabase readingTrackDatabase;
     ReadingTrack mReadingTrack;
-    private static final String DATE_FORMAT = "dd/mm/yyyy";
+    private static final String DATE_FORMAT = "dd/MM/yyyy";
     Date dateObject;
-    private static SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);
+    private static SimpleDateFormat dateFormatter;
+    private DatePickerDialog datePickerDialog ;
 
     public AddReadingTrack(){}
 
@@ -48,21 +53,26 @@ public class AddReadingTrack extends AppCompatActivity {
         setContentView(R.layout.activity_add_reading_track);
         ButterKnife.bind(this);
         mReadingTrack = new ReadingTrack();
+        dateFormatter = new SimpleDateFormat(DATE_FORMAT, Locale.getDefault());
+        mDate.setInputType(InputType.TYPE_NULL);
+        setDateTimeField();
+
+        mDate.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if(hasWindowFocus()){
+                    datePickerDialog.show();
+                }
+                view.clearFocus();
+            }
+        });
 
         mSaveReadingTrackButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 mReadingTrack.setBookTitle(mBookTitle.getText().toString());
-                try {
-                    String stringDate=(mDate.getText().toString());
-                    dateObject = dateFormat.parse(stringDate);
-                    mReadingTrack.setDate(dateObject);
-                } catch (ParseException e) {
-                    e.printStackTrace();
-                }
                 mReadingTrack.setLocation(mLocation.getText().toString());
                 mReadingTrack.setPagesRead(Integer.parseInt(mPages.getText().toString()));
-
 
                 //Simple Runnable and not executor as the User will be add only one Reading track per time.
                 Thread t = new Thread(new Runnable() {
@@ -75,5 +85,18 @@ public class AddReadingTrack extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    private void setDateTimeField() {
+        // Easy DatePicker solution from @NithyaVasudevan+
+        Calendar newCalendar = Calendar.getInstance();
+        datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+                mDate.setText(dateFormatter.format(newDate.getTime()));
+            }
+        },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
     }
 }
